316d055f0d4ddc8a0c0b04903c4dad682720cf1d
Mitchell Cassaday <casmit2884@gmail.com>
2023-09-22 13:11:14 -0400

    Created new views

--- backend/chess/urls.py
+++ backend/chess/urls.py
@@ -1,5 +1,8 @@
-
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('request/',RequestGameAPI.as_view()),
+    path('request/respond/',AcceptGameAPI.as_view()),
+    path('game/<int:id>/move/',validateMoveAPI)
 ]
--- backend/chess/views.py
+++ backend/chess/views.py
@@ -1,3 +1,71 @@
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from chess.models import *
+from api.models import AuthUser
+from .serials import *
+from chess import GameStatus
+from api import ProtectedAPIView
+import chess
 
-# Create your views here.
+class AcceptGameAPI(ProtectedAPIView):
+    def post(self,request):
+        if "id" not in request.data:
+            return Response("No Game Id provided",status.HTTP_400_BAD_REQUEST)
+        try:
+            game = ChessGame.objects.get(pk=request.data["id"])
+        except:
+            return Response("Invalid Game ID provided",status.HTTP_400_BAD_REQUEST)
+        if game.opponent != request.user:
+            return Response("This is not your game to accept...",status.HTTP_401_UNAUTHORIZED)
+        game.startGame()
+        return True
+
+class RequestGameAPI(ProtectedAPIView):
+    authentication_classes = [SessionAuthentication]
+    def delete(self,request):
+        return Response("",status.HTTP_200_OK)
+    def post(self,request):
+        # Side selection
+        if "side" not in request.data:
+            return Response("No side selected, please choose white (0) or black (1)",status.HTTP_400_BAD_REQUEST)
+        data = {
+            "requester": request.user
+        }
+        if "defaultSides" in request.data:
+            if isinstance(request.data["defaultSides"],bool):
+                data["defaultSides"] = request.data["defaultSides"]
+            else:
+                return Response("Invalid side selected",status.HTTP_400_BAD_REQUEST)
+        
+        # Opponent selection
+        if "opponent" not in request.data:
+            # Select Random player
+            data["status"] = GameStatus.WAITING_FOR_OPPONENT
+        else: 
+            # Request a game with a friend
+            data["status"] = GameStatus.PENDING_MATCH_ACCEPTED
+            # Validate user
+            try:
+                opponent = Player.objects.get(pk=request.data["opponent"])
+            except:
+                return Response("Invalid user",status.HTTP_400_BAD_REQUEST)
+            data["opponent"] = opponent
+        # Create Game
+        gameSerial = NewChessGameSerializer(data=data)
+        if gameSerial.is_valid():
+            game = gameSerial.save()
+        else:
+            return Response("Failed to create a new game",status.HTTP_400_BAD_REQUEST)
+        return Response(game.pk,status.HTTP_201_CREATED) 
+    
+@api_view(['GET'])
+def validateMoveAPI(request):
+    """
+    Validates a move
+    """
+    return Response(True)
